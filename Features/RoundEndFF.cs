d8dfc8671d8a1104ef85f3834e374eb7d0565d64
Zereth <70236765+o5zereth@users.noreply.github.com>
2023-05-26 16:01:52 -0400

    Add feature RoundEndFF.

---
+++ Features/RoundEndFF.cs
@@ -0,0 +1,52 @@
+﻿namespace BananaPlugin.Features;
+
+using BananaPlugin.API.Main;
+using BananaPlugin.API.Utils;
+using ExFeatures;
+using Exiled.Events.EventArgs.Server;
+using GameCore;
+
+/// <summary>
+/// The feature responsible for enabling friendly fire when the round ends.
+/// </summary>
+public sealed class RoundEndFF : BananaFeature
+{
+    /// <summary>
+    /// Gets a value indicating whether the base game friendly fire config is enabled.
+    /// </summary>
+    public static bool GetFFConfig => ConfigFile.ServerConfig.GetBool("friendly_fire");
+
+    /// <inheritdoc/>
+    public override string Name => "Round End FF";
+
+    /// <inheritdoc/>
+    public override string Prefix => "end_ff";
+
+    /// <inheritdoc/>
+    protected override void Enable()
+    {
+        ServerHandlers.WaitingForPlayers += this.ResetFFConfig;
+        ServerHandlers.RoundEnded += this.EnableFF;
+    }
+
+    /// <inheritdoc/>
+    protected override void Disable()
+    {
+        ServerHandlers.WaitingForPlayers -= this.ResetFFConfig;
+        ServerHandlers.RoundEnded -= this.EnableFF;
+    }
+
+    private void ResetFFConfig()
+    {
+        Server.FriendlyFire = GetFFConfig;
+
+        BPLogger.Debug("Round end FF was reset.");
+    }
+
+    private void EnableFF(RoundEndedEventArgs ev)
+    {
+        Server.FriendlyFire = true;
+
+        BPLogger.Debug("Round end FF was enabled.");
+    }
+}
---
+++ Usings.cs
@@ -0,0 +1,4 @@
+﻿#pragma warning disable SA1200 // Using directives should be placed correctly
+global using ExFeatures = Exiled.API.Features;
+global using ServerHandlers = Exiled.Events.Handlers.Server;
+#pragma warning restore SA1200 // Using directives should be placed correctly
