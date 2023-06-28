564a33229bcade6bab1030d9385baeb52fe714c6
Zereth <70236765+o5zereth@users.noreply.github.com>
2023-06-28 14:04:06 -0400

    Add TimeHelper class.

---
+++ API/Utils/TimeHelper.cs
@@ -0,0 +1,34 @@
+﻿namespace BananaPlugin.API.Utils;
+
+using System;
+
+/// <summary>
+/// A utility class for retreiving the current time.
+/// </summary>
+public static class TimeHelper
+{
+    /// <summary>
+    /// Gets the current time in US EST form.
+    /// </summary>
+    public static DateTime EstNow
+    {
+        get
+        {
+            DateTime x = DateTime.SpecifyKind(DateTime.UtcNow.AddHours(-5d), DateTimeKind.Unspecified);
+
+            if (x.IsDaylightSavingTime())
+            {
+                x = x.AddHours(1d);
+            }
+
+            return x;
+        }
+    }
+
+    /// <summary>
+    /// Checks if its the specified day of week in US EST form.
+    /// </summary>
+    /// <param name="day">The day to check for.</param>
+    /// <returns>A value indicating whether it is the current day in US EST form.</returns>
+    public static bool IsDayOfWeek(this DayOfWeek day) => EstNow.DayOfWeek == day;
+}
