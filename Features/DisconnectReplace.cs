bf8f08387f2cb67b78b67b09f34a153d8f313224
Zereth <70236765+o5zereth@users.noreply.github.com>
2023-07-14 18:37:49 -0400

    Add extra DisconnectReplace placeholder.

--- Features/DisconnectReplace.cs
+++ Features/DisconnectReplace.cs
@@ -2,9 +2,13 @@
 
 using BananaPlugin.API.Main;
 using BananaPlugin.API.Utils;
+using Exiled.Events.EventArgs.Player;
 using HarmonyLib;
 using NorthwoodLib.Pools;
+using PlayerRoles.Spectating;
+using PlayerStatsSystem;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using static HarmonyLib.AccessTools;
@@ -34,7 +38,48 @@ public sealed class DisconnectReplace : BananaFeature
     {
     }
 
-    // [HarmonyPatch(typeof(CustomNetworkManager), nameof(CustomNetworkManager.OnServerDisconnect))]
+    private static bool FilterHubs(ReferenceHub left, ReferenceHub toSelect)
+    {
+        return toSelect.roleManager.CurrentRole is SpectatorRole spec
+
+            // Northwood skill issue. https://trello.com/c/Q7ziLNP3/4660-player-3rd-person-view
+            && spec.SyncedSpectatedNetId != left.netId;
+    }
+
+    private static float GetDeathTime(ReferenceHub hub)
+    {
+        return hub.roleManager.CurrentRole is SpectatorRole spec
+            ? spec.ActiveTime
+            : 0f;
+    }
+
+    private void Left(LeftEventArgs ev)
+    {
+        // Add proper role checking here.
+        // Ex: tutorials should not be replaced.
+        //
+        // However, custom roles should specify
+        // if they can be replaced.
+        return;
+
+        ReferenceHub left = ev.Player.ReferenceHub;
+        ReferenceHub? available =
+            ReferenceHub.AllHubs
+            .OrderByDescending(GetDeathTime)
+            .FirstOrDefault(x => FilterHubs(left, x));
+
+        // Nobody available to replace
+        // the disconnected player.
+        if (available is null)
+        {
+            left.playerStats.DealDamage(new UniversalDamageHandler(-1f, DeathTranslations.Unknown));
+            return;
+        }
+
+        // Apply replacement here.
+    }
+
+    [HarmonyPatch(typeof(CustomNetworkManager), nameof(CustomNetworkManager.OnServerDisconnect))]
     private static class RemoveDisonnectDropPatch
     {
         // This patch removes default disconnectDrop implementation.
