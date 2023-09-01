9d4de1ba8b1c88a08ba65460119afa1a56c498d0
Zereth <70236765+o5zereth@users.noreply.github.com>
2023-09-01 15:47:58 -0400

    [1.4.0] Update

--- Versioning.cs
+++ Versioning.cs
@@ -21,7 +21,7 @@ public static class Versioning
     /// <summary>
     /// The string representation of the current assembly version.
     /// </summary>
-    public const string VersionString = "1.3.1";
+    public const string VersionString = "1.4.0";
 
     /// <summary>
     /// The extension of the current assembly version.
