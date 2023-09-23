f98e9ac23ec87519a0ef52461cba0339e5331a15
Zereth <70236765+o5zereth@users.noreply.github.com>
2023-09-23 14:18:35 -0400

    optimize ToClientUShorts

--- Extensions/QuaternionExtensions.cs
+++ Extensions/QuaternionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿namespace BananaPlugin.Extensions;
 
+using System;
 using UnityEngine;
 
 /// <summary>
@@ -8,47 +9,29 @@ using UnityEngine;
 public static class QuaternionExtensions
 {
     /// <summary>
-    /// Converts a specified rotation to a clients rotation values in ushort form.
+    /// Converts a quaternion rotation into a pair of unsigned shorts (horizontal, vertical) for client representation.
     /// </summary>
-    /// <param name="rotation">The rotation to convert.</param>
-    /// <returns>A <see cref="System.ValueTuple{T1,T2}"/> representing the horizontal and vertical ushort values converted, in order, respectively.</returns>
+    /// <param name="rotation">The quaternion rotation to convert.</param>
+    /// <returns>A tuple containing the horizontal and vertical angles as unsigned shorts.</returns>
     public static (ushort horizontal, ushort vertical) ToClientUShorts(this Quaternion rotation)
     {
-        if (rotation.eulerAngles.z != 0f)
+        const float ToHorizontal = ushort.MaxValue / 360f;
+        const float ToVertical = ushort.MaxValue / 176f;
+
+        float fixVertical = -rotation.eulerAngles.x;
+
+        if (fixVertical < -90f)
         {
-            rotation = Quaternion.LookRotation(rotation * Vector3.forward, Vector3.up);
+            fixVertical += 360f;
+        }
+        else if (fixVertical > 270f)
+        {
+            fixVertical -= 360f;
         }
 
-        float outfHorizontal = rotation.eulerAngles.y;
-        float outfVertical = -rotation.eulerAngles.x;
+        float horizontal = Mathf.Clamp(rotation.eulerAngles.y, 0f, 360f);
+        float vertical = Mathf.Clamp(fixVertical, -88f, 88f) + 88f;
 
-        if (outfVertical < -90f)
-        {
-            outfVertical += 360f;
-        }
-        else if (outfVertical > 270f)
-        {
-            outfVertical -= 360f;
-        }
-
-        return (ToHorizontal(outfHorizontal), ToVertical(outfVertical));
-    }
-
-    private static ushort ToHorizontal(float horizontal)
-    {
-        const float ToHorizontal = 65535f / 360f;
-
-        horizontal = Mathf.Clamp(horizontal, 0f, 360f);
-
-        return (ushort)Mathf.RoundToInt(horizontal * ToHorizontal);
-    }
-
-    private static ushort ToVertical(float vertical)
-    {
-        const float ToVertical = 65535f / 176f;
-
-        vertical = Mathf.Clamp(vertical, -88f, 88f) + 88f;
-
-        return (ushort)Mathf.RoundToInt(vertical * ToVertical);
+        return ((ushort)Math.Round(horizontal * ToHorizontal), (ushort)Math.Round(vertical * ToVertical));
     }
 }
