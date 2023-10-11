4f3735fb26a3388be4d89e94653fe7f7a8ff2138
Zereth <70236765+o5zereth@users.noreply.github.com>
2023-10-11 09:04:53 -0400

    Make pushing 3x stronger on chaotic thursdays.

--- Features/ChaoticThursdays.cs
+++ Features/ChaoticThursdays.cs
@@ -2,6 +2,12 @@
 
 using BananaPlugin.API.Main;
 using BananaPlugin.API.Utils;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using static HarmonyLib.AccessTools;
+
+#pragma warning disable SA1118 // Parameter should not span multiple lines
 
 /// <summary>
 /// The main feature responsible for chaotic thursdays.
@@ -10,12 +16,18 @@ public sealed class ChaoticThursday : BananaFeature
 {
     private ChaoticThursday()
     {
+        Instance = this;
     }
+
+    /// <summary>
+    /// Gets the chaotic thursday instance.
+    /// </summary>
+    public static ChaoticThursday? Instance { get; private set; }
 
     /// <summary>
     /// Gets a value indicating whether chaotic thursday is active.
     /// </summary>
-    public static bool IsActive { get; private set; }
+    public static bool IsActive { get; private set; } = false;
 
     /// <inheritdoc/>
     public override string Name => "Chaotic Thursdays";
@@ -33,6 +45,8 @@ public sealed class ChaoticThursday : BananaFeature
     protected override void Disable()
     {
         ExHandlers.Server.WaitingForPlayers -= this.WaitingForPlayers;
+
+        IsActive = false;
     }
 
     private void WaitingForPlayers()
@@ -41,5 +55,41 @@ public sealed class ChaoticThursday : BananaFeature
         // once a round is ready, so it
         // doesn't enable mid-round.
         IsActive = Versioning.LocalBuild || System.DayOfWeek.Thursday.IsDayOfWeek();
+    }
+
+    [HarmonyPatch]
+    private static class PushForcePatch
+    {
+        [HarmonyPrepare]
+        private static bool Init()
+        {
+            return TypeByName("Push.Config") is not null;
+        }
+
+        [HarmonyPatch("Push.Config", nameof(Push.Config.PushForce), MethodType.Getter)]
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            instructions.BeginTranspiler(out List<CodeInstruction> newInstructions);
+
+            int index = newInstructions.FindLastIndex(x => x.opcode == OpCodes.Ret);
+
+            Label skipLabel = generator.DefineLabel();
+
+            newInstructions[index].labels.Add(skipLabel);
+
+            newInstructions.InsertRange(index, new CodeInstruction[]
+            {
+                // if (ChaoticThursday.IsActive)
+                // {
+                //     __result *= 3f;
+                // }
+                new(OpCodes.Call, PropertyGetter(typeof(ChaoticThursday), nameof(IsActive))),
+                new(OpCodes.Brfalse_S, skipLabel),
+                new(OpCodes.Ldc_R4, 3f),
+                new(OpCodes.Mul),
+            });
+
+            return newInstructions.FinishTranspiler();
+        }
     }
 }
